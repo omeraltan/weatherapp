ead32e5e6fb9e0dc8704473a5fd743b0cc20ffb3
Ömer ALTAN <omeraltan66@gmail.com>
2022-06-25 17:41:42 +0300

    Yapılan Son Değişiklikler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [temp, setTemp] = useState('');
+  const [desc, setDesc] = useState('');
+  const [icon, setIcon] = useState('');
+  const [isReady, setReady] = useState(false);
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
 
+  React.useEffect(() => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c36b03a963176b9a639859e6cf279299&units=metric')
+      .then(result => result.json())
+      .then(jsonresult => {
+        setTemp(jsonresult.main.temp);
+        setDesc(jsonresult.weather[0].main);
+        setIcon(jsonresult.weather[0].icon);
+        setReady(true);
+      })
+      .catch(err => console.error(err));
+  }, []);
+  const fetchData = () => {
+    fetch(`https://api.github.com/search/repositories?q=${keyword}`)
+      .then(response => response.json())
+      .then(data => setData(data.items))
+      .catch(err => console.error(err));
+  };
+  if (isReady) {
+    return (
+      <div className="App">
+        <div>
+          <p>Temperature: {temp} °C</p>
+          <p>Description: {desc}</p>
+          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="Weather icon" />
+        </div>
+        <div>
+          <input value={keyword} onChange={e => setKeyword(e.target.value)} />
+          <button onClick={fetchData}>Fetch</button>
+          <table>
+            <tbody>
+              {data.map(repo => (
+                <tr>
+                  <td>{repo.full_name}</td>
+                  <td>
+                    <a href={repo.html_url}>{repo.html_url}</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+}
 export default App;
